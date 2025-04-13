b27b8cf3b8ce8c88e0903743a0108086426e3501
medde142 <m.stapelbroek@web.de>
2025-04-13 11:33:32 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# sbx-test2
+# SBX Projekt App (mit Passwortschutz)
+
+Benutzername: sbx
+Passwort: 1234
---
+++ components/ui/card.tsx
@@ -0,0 +1,2 @@
+export const Card = ({ children }) => <div className='border rounded-xl p-4 bg-white'>{children}</div>;
+export const CardContent = ({ children }) => <div>{children}</div>;
